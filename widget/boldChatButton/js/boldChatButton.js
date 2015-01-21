e674b008eda2582796e1d1469ec2b68880b6dea1
Ian Davis <idavis@idavismacbook3.home>
2015-01-21 09:59:41 +0000

    First commit

---
+++ widget/boldChatButton/js/boldChatButton.js
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview BoldChat Button Invite
+ * Option.
+ * @author ian.davis@oracle.com
+ */
+
+define(
+  //-------------------------------------------------------------------
+  // PACKAGE NAME
+  //-------------------------------------------------------------------
+  'boldChatButton',
+
+  //-------------------------------------------------------------------
+  // DEPENDENCIES
+  //-------------------------------------------------------------------
+  ['knockout','ccLogger'],
+
+  //-------------------------------------------------------------------
+  // MODULE DEFINITION
+  //-------------------------------------------------------------------
+  function(ko, ccLogger) {
+
+    "use strict";
+
+    var self;
+
+    return {
+
+      onLoad: function(widgetModel) {
+        self = widgetModel;
+        self.bcScript = document.location.protocol + '//vmss.boldchat.com/aid/' + self.bcAccountID() + '/bc.vms4/vms.js';
+        if(self.bcAccountID() === "") ccLogger.info("BoldChat Widget needs Account ID configured.");
+        if(self.bcWebsiteID() === "") ccLogger.info("BoldChat Widget needs Website ID configured.");
+      },
+
+      beforeAppear: function(page) {
+
+        if(self.bcAccountID() !== "" && self.bcWebsiteID() !== "") {
+          window._bcvma = [];
+          _bcvma.push(["setAccountID", self.bcAccountID()]);
+          _bcvma.push(["setParameter", "WebsiteID", self.bcWebsiteID()]);
+          _bcvma.push(["setParameter", "VisitRef", self.user().id()]);
+          _bcvma.push(["setParameter", "VisitName", self.user().firstName() + " " + self.user().lastName()]);
+          _bcvma.push(["setParameter", "VisitEmail", self.user().emailAddress()]);
+          _bcvma.push(["addStatic", {type: "chat", bdid: self.bcButtonID(), id: 'bc-' + self.id()}]);
+
+          if(window._bcvmb) require.undef(self.bcScript);
+          require([self.bcScript]);
+        }
+      }
+    };
+  }
+);
